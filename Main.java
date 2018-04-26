88071cba8b1c7611491abefc06b2eaf75ba21043
Kostya Sharapov <l.mugivara@bk.ru>
2018-04-26 17:17:14 +0700

    Made some changes

--- Main.java
+++ Main.java
@@ -3,6 +3,14 @@ package com.testTools.demo.start;
 public class Main {
 
     public static void main(String[] args) {
+      String [] array = new String[]{"hello","my","first","repository","on java file"};
+
+
+      for (int a; a<array.lenght; a++ ) {
+          System.out.println(array[a]);//выводит его индексы
+      }
+
+
 	        System.out.println("hello my first repository on java file");
     }
 }
